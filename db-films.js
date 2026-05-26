760e352edd2af70954981bdf5a91bddc8c957da7
FloKBCode <florence.korebelle@ynov.com>
2026-05-26 13:27:56 +0200

    feat: add posters and rename to Pellicule

--- db-films.js
+++ db-films.js
@@ -1,16 +1,16 @@
 const films = [
-  { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010, genre: "Science-fiction" },
-  { id: 2, title: "Le Parrain", director: "Francis Ford Coppola", year: 1972, genre: "Drame" },
-  { id: 3, title: "Interstellar", director: "Christopher Nolan", year: 2014, genre: "Science-fiction" },
-  { id: 4, title: "Parasite", director: "Bong Joon-ho", year: 2019, genre: "Thriller" },
-  { id: 5, title: "Titanic", director: "James Cameron", year: 1997, genre: "Romance" },
-  { id: 6, title: "The Dark Knight", director: "Christopher Nolan", year: 2008, genre: "Action" },
-  { id: 7, title: "Pulp Fiction", director: "Quentin Tarantino", year: 1994, genre: "Crime" },
-  { id: 8, title: "Forrest Gump", director: "Robert Zemeckis", year: 1994, genre: "Drame" },
-  { id: 9, title: "Le Roi Lion", director: "Roger Allers", year: 1994, genre: "Animation" },
-  { id: 10, title: "Avengers: Endgame", director: "Anthony et Joe Russo", year: 2019, genre: "Action" },
-  { id: 11, title: "Spirited Away", director: "Hayao Miyazaki", year: 2001, genre: "Animation" },
-  { id: 12, title: "Matrix", director: "Les Wachowski", year: 1999, genre: "Science-fiction" },
+  { id: 1, title: "Inception", director: "Christopher Nolan", year: 2010, genre: "Science-fiction", poster: "https://image.tmdb.org/t/p/w500/9gk7adHYeDvHkCSEqAvQNLV5Uge.jpg" },
+  { id: 2, title: "Le Parrain", director: "Francis Ford Coppola", year: 1972, genre: "Drame", poster: "https://image.tmdb.org/t/p/w500/3bhkrj58Vtu7enYsLegHnDmni2M.jpg" },
+  { id: 3, title: "Interstellar", director: "Christopher Nolan", year: 2014, genre: "Science-fiction", poster: "https://image.tmdb.org/t/p/w500/gEU2QniE6E77NI6lCU6MxlNBvIx.jpg" },
+  { id: 4, title: "Parasite", director: "Bong Joon-ho", year: 2019, genre: "Thriller", poster: "https://image.tmdb.org/t/p/w500/7IiTTgloJzvGI1TAYymCfbfl3vT.jpg" },
+  { id: 5, title: "Titanic", director: "James Cameron", year: 1997, genre: "Romance", poster: "https://image.tmdb.org/t/p/w500/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg" },
+  { id: 6, title: "The Dark Knight", director: "Christopher Nolan", year: 2008, genre: "Action", poster: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg" },
+  { id: 7, title: "Pulp Fiction", director: "Quentin Tarantino", year: 1994, genre: "Crime", poster: "https://image.tmdb.org/t/p/w500/d5iIlFn5s0ImszYzBPb8JPIfbXD.jpg" },
+  { id: 8, title: "Forrest Gump", director: "Robert Zemeckis", year: 1994, genre: "Drame", poster: "https://image.tmdb.org/t/p/w500/arw2vcBveWOVZr6pxd9XTd1TdQa.jpg" },
+  { id: 9, title: "Le Roi Lion", director: "Roger Allers", year: 1994, genre: "Animation", poster: "https://image.tmdb.org/t/p/w500/sKCr78MXSLixwmZ8DyJLrpMsd15.jpg" },
+  { id: 10, title: "Avengers: Endgame", director: "Anthony et Joe Russo", year: 2019, genre: "Action", poster: "https://image.tmdb.org/t/p/w500/or06FN3Dka5tukK1e9sl16pB3iy.jpg" },
+  { id: 11, title: "Spirited Away", director: "Hayao Miyazaki", year: 2001, genre: "Animation", poster: "https://image.tmdb.org/t/p/w500/39wmItIWsg5sZMyRUHLkWBcuVCM.jpg" },
+  { id: 12, title: "Matrix", director: "Les Wachowski", year: 1999, genre: "Science-fiction", poster: "https://image.tmdb.org/t/p/w500/f89U3ADr1oiB1s9GkdPOEpXUk5H.jpg" },
 ];
 
 module.exports = films;
